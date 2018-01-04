130c80cf8ecbc1efd85073d4bbd4d5ff6b1b656f
Michael Albrecht <michael.albrecht@st.ovgu.de>
2018-01-04 17:03:15 +0100

    Added changed parameters from EEPROM

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -531,7 +531,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 92.635, 92.635, 4000, 660 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 660 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -556,9 +556,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          400    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   600    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          300    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  800    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   500    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
